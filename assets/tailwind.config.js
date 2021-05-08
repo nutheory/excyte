50e902733a7ab9e6c0b3a983382e2a7aaf39cf64
Derek Rush <drush81@gmail.com>
2021-05-07 21:00:31 -0700

    images, activities scaffold, templating working

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -28,6 +28,20 @@ module.exports = {
     extend: {
       height: {
         '18': '4.5rem',
+      },
+      fontSize: {
+        'tiny': '.65rem',
+      },
+      spacing: {
+        '2/3': '66.666667%'
+      },
+      maxWidth: {
+        '8xl': '90rem',
+      },
+      animations: {
+        ping: 'ping 1s cubic-bezier(0, 0, 0.2, 1) infinite',
+        select: 'select 2s cubic-bezier(0, 0, 0.2, 1)',
+        deselect: 'deselect 2s cubic-bezier(0, 0, 0.2, 1)',
       }
     }
   },

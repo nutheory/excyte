7b730eda10b95da767e5384aa66b0f9b16779205
Derek Rush <drush81@gmail.com>
2021-08-02 04:41:45 -0700

    notifier fix

--- lib/excyte/accounts/user_notifier.ex
+++ lib/excyte/accounts/user_notifier.ex
@@ -77,29 +77,73 @@ defmodule Excyte.Accounts.UserNotifier do
   """
   def deliver_confirmation_instructions(user, url) do
     text_body = """
+    Hello!
+    Welcome from the entire Excyte CMA team! We are here to help anyway possible.
+    We offer daily webinars, 1st class customer support and a mobile first solution
+    like no other presentation software.
 
-    ==============================
+    Your free 30 day trial ends 07/26/2016. Information to your membership is located
+    in the settings on your dashboard under the Account button. You can change your
+    email/password or cancel your account there. We will remind you 10 days prior
+    to your account being charged in case you want to make any changes to your plan
+    or end the free trial.
 
-    Hi #{user.email},
+    We are thrilled to have you aboard and look forward to your feedback. We believe
+    the best way to get better is to grow together.
 
-    You can confirm your account by visiting the url below:
+    We appreciate you,
 
-    #{@base_url}#{url}
-
-    If you didn't create an account with us, please ignore this.
-
-    ==============================
+    Excyte CMA Team
     """
 
     html_body = """
-    Hi #{user.email},<br/></br/>
-    You can confirm your account by visiting the url below:<br/></br/>
-    <a href="#{@base_url}#{url}" target="_blank">#{@base_url}#{url}</a><br/></br/>
-    If you didn't create an account with us, please ignore this.<br/></br/>
+    <div style="max-width:'600px';">
+    <p>Hello!</p>
+
+    <p>Welcome from the entire Excyte CMA team! We are here to help anyway possible.
+    We offer daily webinars, 1st class customer support and a mobile first solution
+    like no other presentation software.</p>
+
+    <p>Your free 30 day trial ends 07/26/2016. Information to your membership is located
+    in the settings on your dashboard under the Account button. You can change your
+    email/password or cancel your account there. We will remind you 10 days prior to
+    your account being charged in case you want to make any changes to your plan or
+    end the free trial.</p>
+
+    <p>We are thrilled to have you aboard and look forward to your feedback. We believe
+    the best way to get better is to grow together.</p>
+
+    <p>We appreciate you,</p>
+
     #{@html_signature}
+    </div>
     """
 
-    deliver(user.email, "Please confirm your account", text_body, html_body)
+    deliver(user.email, "Welcome to Excyte CMA", text_body, html_body)
+    # text_body = """
+
+    # ==============================
+
+    # Hi #{user.email},
+
+    # You can confirm your account by visiting the url below:
+
+    # #{@base_url}#{url}
+
+    # If you didn't create an account with us, please ignore this.
+
+    # ==============================
+    # """
+
+    # html_body = """
+    # Hi #{user.email},<br/></br/>
+    # You can confirm your account by visiting the url below:<br/></br/>
+    # <a href="#{@base_url}#{url}" target="_blank">#{@base_url}#{url}</a><br/></br/>
+    # If you didn't create an account with us, please ignore this.<br/></br/>
+    # #{@html_signature}
+    # """
+
+    # deliver(user.email, "Please confirm your account", text_body, html_body)
   end
 
   def deliver_invitation_instructions(user, url) do

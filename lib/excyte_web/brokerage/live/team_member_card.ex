05fd90cd2093c00bf4240fd62f5bf2090be63820
Derek Rush <drush81@gmail.com>
2021-08-03 16:48:49 -0700

    webinar

--- lib/excyte_web/brokerage/live/team_member_card.ex
+++ lib/excyte_web/brokerage/live/team_member_card.ex
@@ -6,8 +6,10 @@ defmodule ExcyteWeb.Brokerage.TeamMember do
   def render(assigns), do: BrokerageView.render("manage_team.html", assigns)
 
   def update(assigns, socket) do
-
+    {:ok, assign(socket,
+      member: assigns.member,
+      show_controls: false
+    )}
   end
-
 
 end

ac362fa840652ea6ab038c408b0ae36f89dec16e
Derek Rush <drush81@gmail.com>
2021-08-04 17:42:08 -0700

    legal and webinar update

--- lib/excyte_web/public/live/privacy.ex
+++ lib/excyte_web/public/live/privacy.ex
@@ -1,8 +1,8 @@
-defmodule ExcyteWeb.Pricing do
+defmodule ExcyteWeb.Privacy do
   use ExcyteWeb, :live_public_view
   alias ExcyteWeb.{PublicView}
 
-  def render(assigns), do: PublicView.render("pricing.html", assigns)
+  def render(assigns), do: PublicView.render("privacy.html", assigns)
 
   @impl true
   @spec mount(any, any, any) :: {:ok, any}

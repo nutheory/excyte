2a7e8745be91a44f0c997f79fba0a32e21fb33b5
Derek Rush <drush81@gmail.com>
2021-10-08 15:33:51 -0700

    new design stuff

--- priv/repo/migrations/20211008005019_section_modify.exs
+++ priv/repo/migrations/20211008005019_section_modify.exs
@@ -2,11 +2,9 @@ defmodule Excyte.Repo.Migrations.SectionDelete do
   use Ecto.Migration
 
   def change do
-    alter table(:sections) do
+    alter table(:section_templates) do
       add :enabled, :boolean, default: true
       add :content, :map, default: %{}
-      add :component_name, :string
-      modify :insight_id, references(:insights, on_delete: :delete_all), from: references(:insights)
     end
   end
 end

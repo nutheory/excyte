1a47ce9a6b3fefbd478ab85c0a5d5c5f87cec42c
Derek Rush <drush81@gmail.com>
2021-10-15 05:02:21 -0700

    lots of new design fixes

--- priv/repo/migrations/20211008005020_section_delete.exs
+++ priv/repo/migrations/20211008005020_section_delete.exs
@@ -3,10 +3,10 @@ defmodule Excyte.Repo.Migrations.SectionDelete do
 
   def change do
     alter table(:sections) do
-      # add :enabled, :boolean, default: true
-      # add :content, :map, default: %{}
-      # add :component_name, :string
-      # modify :insight_id, references(:insights, on_delete: :delete_all), from: references(:insights)
+      add :enabled, :boolean, default: true
+      add :content, :map, default: %{}
+      add :component_name, :string
+      modify :insight_id, references(:insights, on_delete: :delete_all), from: references(:insights)
     end
   end
 end
